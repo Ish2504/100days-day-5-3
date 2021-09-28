67ea74b5d73a74c523d4249df4ef0938dddf96a2
Ish2504 <87877194+Ish2504@users.noreply.github.com>
2021-09-28 19:30:38 +0000

    1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #Write your code below this row 👇
+count = 0
+for number in range(2, 101, 2):  
+  count += number
 
+print(count)
+
+count1 = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    count1 += number
+
+print(count1)
